a85c0554ee0e844ea203280bdcb4d239870d67b6
Carlos Carvalho <carval.carlos@outlook.com>
2025-03-20 15:57:40 +0000

    Initial Commit

---
+++ src/main/java/com/cacolibrarycorp/library/bookreservation/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.cacolibrarycorp.library.bookreservation.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
